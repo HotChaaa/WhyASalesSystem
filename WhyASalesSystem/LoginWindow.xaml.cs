b8333847bb4b56dbe4089e8cc6802f42f780bbc5
HotChaaa <aikahotdeath@gmail.com>
2026-07-18 13:33:08 +0700

    base-v0.01

---
+++ WhyASalesSystem/LoginWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WhyASalesSystem
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                btnLogin_Click(sender, e);
+            }
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Password;
+
+            if (username == "test" && password == "1234")
+            {
+                MainWindow mainWindow = new MainWindow(username);
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Username หรือ Password ไม่ถูกต้อง",
+                                "Login Failed",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ WhyASalesSystem/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WhyASalesSystem.Views;
+
+namespace WhyASalesSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string username)
+        {
+            InitializeComponent();
+
+            txtCurrentUser.Text = username;
+
+            MainContent.Content = new DashboardView();
+        }
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+
+            this.Close();
+        }
+
+        private void btnDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new DashboardView();
+        }
+
+        private void btnSale_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new SaleView();
+        }
+
+        private void btnInventory_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new InventoryView();
+        }
+    }
+}
